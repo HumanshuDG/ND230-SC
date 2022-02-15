1cb6e46680cd3ae05ea805ada3663842c5144958
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2022-02-15 18:41:01 +0530

    Create quiz.py

---
+++ L8.3/Testing your models/quiz.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+data = np.asarray(pd.read_csv('data.csv', header=None))
+X = data[:,0:2]
+y = data[:,2]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+model = DecisionTreeClassifier()
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
